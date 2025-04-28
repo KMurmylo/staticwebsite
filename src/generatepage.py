6a23a35626f66dfca6140d6a696588686b8435d3
Bob <BobsEmail>
2025-04-28 22:20:17 +0200

    Built webpage

--- src/generatepage.py
+++ src/generatepage.py
@@ -2,7 +2,7 @@ import os
 from markdowntohtml import markdown_to_html_node
 from extracttitle import extract_title
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath="/"):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
     with open(from_path, 'r',encoding="utf-8") as f:
@@ -15,5 +15,9 @@ def generate_page(from_path, template_path, dest_path):
     title=extract_title(source)
     result=template.replace("{{ Title }}",title)
     result=result.replace("{{ Content }}",page.to_html())
+
+    result = result.replace('href="/', f'href="{basepath}')
+    result = result.replace('src="/', f'src="{basepath}')
+    
     with open(dest_path, 'w',encoding="utf-8") as f:
         f.write(result)
--- src/main.py
+++ src/main.py
@@ -1,18 +1,19 @@
 from textnode import TextNode,TextType
 import os
 import shutil
+import sys
 from generatepage import generate_page
 from extracttitle import extract_title
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath="/"):
     for file in os.listdir(dir_path_content):
         
         if os.path.isdir(os.path.join(dir_path_content,file)):
                 os.makedirs(os.path.join(dest_dir_path,file),exist_ok=True)
-                generate_pages_recursive(os.path.join(dir_path_content,file),template_path, os.path.join(dest_dir_path,file))
+                generate_pages_recursive(os.path.join(dir_path_content,file),template_path, os.path.join(dest_dir_path,file),basepath)
         if os.path.isfile(os.path.join(dir_path_content,file)):
             if file.endswith(".md"):
-                generate_page(os.path.join(dir_path_content,file), template_path, os.path.join(dest_dir_path,file).rstrip("md")+"html")
+                generate_page(os.path.join(dir_path_content,file), template_path, os.path.join(dest_dir_path,file).rstrip("md")+"html",basepath)
 
     
 def copyall(src,dst):
@@ -27,12 +28,18 @@ def main():
     textnode=TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
     print(textnode)
     
-    if os.path.exists("public"):
-        shutil.rmtree("public")
-    os.mkdir("public")
+    if os.path.exists("docs"):
+        shutil.rmtree("docs")
+    os.mkdir("docs")
 
-    copyall("static","public")
-    generate_pages_recursive("content","template.html","public")
+    copyall("static","docs")
+    
+    if len(sys.argv) >= 2:
+     basepath = sys.argv[1]
+    else:
+        basepath = "/"
+
+    generate_pages_recursive("content", "template.html", "docs", basepath)
     #generate_page("content/index.md","template.html","public/index.html")
     return
 
